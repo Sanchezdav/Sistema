f91b169a6e92c9d3192f657c23f8dc11a6f12e17
David Sanchez <sanchez.dav90@gmail.com>
2014-03-03 23:18:24 -0600

    Cuarto Commit
    Se mejoro el html completo, cambio el ajax por el original

--- Mensajeria/static/js/script.js
+++ Mensajeria/static/js/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function(){
 
-   $('#aceptar').on('clic',refrescar);
-
-});
-
-
-function refrescar()
-{
-	setInterval(function(){
+   setInterval(function(){
    		$.ajax({
 		   	url: '/table',
 		   	success:function(data){
 		   		$('#datagrid1').html(data)
 		   	}
 		 });
-   },3e3);
-}
+   },30000);
+
+});
