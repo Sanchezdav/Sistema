16a667ef909486c832cf18d4a0c1bd24400f3322
David Sanchez <sanchez.dav90@gmail.com>
2014-03-13 11:28:58 -0600

    Quinto commit
    Se corrigieron estilos
    Se agregaron notificaciones de escritorio al enviar un servicio
    con el archivo notificacion.js

---
+++ Mensajeria/static/js/notificacion.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    $("#aceptar").click(function () {
+        SimpleNotification.notify({
+            icon: 'http://code.vikaskbh.com/javascript-logo-small.png',
+            title: 'Nuevo Servicio',
+            description: 'Tienes un nuevo servicio por favor verifica'
+        }).onClickOpen("http://10.33.168.1:8000").show();
+    });
+
+});
+
+SimpleNotification = window.SimpleNotification || {};
+SimpleNotification = (function () {
+    var notification;
+    var permitted = window.webkitNotifications.checkPermission() == 0 ? true : false;
+
+    var defaults = {
+        icon: 'http://code.vikaskbh.com/javascript-logo-small.png',
+        title: 'Nuevo Servicio',
+        description: 'Tienes un nuevo servicio por favor verifica'
+    }
+
+    var showNotification = function (options) {
+        if (!permitted) {
+            window.webkitNotifications.requestPermission();
+            return;
+        }
+        var settings = jQuery.extend({}, defaults, options);
+        notification = window.webkitNotifications.createNotification(
+            settings.icon,
+            settings.title,
+            settings.description
+        );
+        return this;
+    }
+
+    var clickEvent = function (url) {
+        notification.onclick = function () {
+            window.open(url);
+            notification.close();
+        }
+        return this;
+    }
+
+    var showEvent = function () {
+        notification.show();
+    }
+
+    return {
+        notify: showNotification,
+        onClickOpen: clickEvent,
+        show: showEvent
+    }
+
+})();
